f04f3aee93a41508f025eafe7b60d14c5fcdb5c2
Guava <zoeGuava@gmail.com>
2021-06-07 01:00:35 +0800

    HeroService / MessageService
    RxJS, Observable, subscribe

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes'
+import { MessageService } from './message.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
+  constructor(private messageService: MessageService) {}
+  getHeroes(): Observable<Hero[]> {
+    // of(HEROES) 會 return 一個 Observable<Hello[]>
+    // 他會 emit 一個模擬 heroes 資料的 single value
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes')
+    return heroes;
   }
-  constructor() { }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from './../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +9,25 @@ import { HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
   selectedHero?: Hero;
+  heroes: Hero[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // 1. 定義一個 private 屬性的 heroService
+  // 2. 把它標記為 HeroService 的注入點
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
+  ngOnInit() {
+    this.getHeroes();
   }
-
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Select hero id=${hero.id}`);
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
   }
 }
 
